2cc1de87c35edb75387e63be5f8c428de7df15c7
Igor Bedesqui <igorbedesqui@gmail.com>
2023-06-22 03:04:43 +0100

    feat: return queryParams as part of response

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,12 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const { url } = request;
+  const query = new URL(url).searchParams;
+
   const data = {
     status: 'sent lol',
+    query: query.get('query'),
   };
 
   return NextResponse.json({ data });
