c87ded8891c55991572b037fa99255508d9db16f
Igor Bedesqui <igorbedesqui@gmail.com>
2023-06-22 03:40:15 +0100

    feat: add channel and stream id params

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,8 +6,12 @@ export async function GET(request: NextRequest) {
   const { url } = request;
   const params = new URL(url).searchParams;
 
+  // TODO: Store this somewhere
+
   const data = {
     status: 'sent lol',
+    channel: params.get('channel'),
+    stream_id: params.get('stream_id'),
     title: params.get('title'),
     time: params.get('time'),
   };
